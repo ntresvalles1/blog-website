7aaabdcd9791f0d9a6b0f0383381c0a536c54053
Nicole Tresvalles <nicoltresvalles01@gmail.com>
2022-07-18 11:25:46 -0700

    allows user to upload images

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authenticationRoute = require("./routes/authentication");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/category");
+const multer = require("multer");
 
 
 dotenv.config();
@@ -18,6 +19,21 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("connected to mongo"))
   .catch(err=>console.log(err));
 
+const storage = multer.diskStorage({
+  destination:(req,file,cb) => {
+    cb(null,"images");
+  },
+  filename:(req,file,cb) => {
+    cb(null,req.body.name);
+  },
+});  //takes our file and its going to be inside images 
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"),(req,res) => {
+  res.status(200).json("File successfully uploaded")
+
+});//sends our image data from postman
+
 app.use("/api/authentication", authenticationRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
@@ -26,4 +42,4 @@ app.use("/api/category", categoryRoute);
 
 app.listen("4000", () =>{
     console.log("Backend is running.")
-})
+});
